94c74528a54f9cdb4160a3467e40a55ffd52656a
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-05-19 09:08:33 +0900

    Initial Commit

---
+++ BLERandomNumberSender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLERandomNumberSender
+//
+//  Created by Osamu HIraoka on 2021/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
